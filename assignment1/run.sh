63900b03bb5584624eeb741ca81fb7e41fbe77ed
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2022-10-24 17:06:13 +0100

    run script runs compiled jar remotely in cluster

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -1,16 +1,17 @@
-java \
-    --add-opens=java.base/java.lang=ALL-UNNAMED \
-    --add-opens=java.base/java.lang.invoke=ALL-UNNAMED \
-    --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
-    --add-opens=java.base/java.io=ALL-UNNAMED \
-    --add-opens=java.base/java.net=ALL-UNNAMED \
-    --add-opens=java.base/java.nio=ALL-UNNAMED \
-    --add-opens=java.base/java.util=ALL-UNNAMED \
-    --add-opens=java.base/java.util.concurrent=ALL-UNNAMED \
-    --add-opens=java.base/java.util.concurrent.atomic=ALL-UNNAMED \
-    --add-opens=java.base/sun.nio.ch=ALL-UNNAMED \
-    --add-opens=java.base/sun.nio.cs=ALL-UNNAMED \
-    --add-opens=java.base/sun.security.action=ALL-UNNAMED \
-    --add-opens=java.base/sun.util.calendar=ALL-UNNAMED \
-    --add-opens=java.security.jgss/sun.security.krb5=ALL-UNNAMED \
-    -jar target/assignment1-1.0-SNAPSHOT.jar $@
+scp -i ~/.ssh/cloud ./target/assignment1-1.0-SNAPSHOT.jar cc-group2@caelum-109.cl.cam.ac.uk:~
+ssh -i ~/.ssh/cloud cc-group2@caelum-109.cl.cam.ac.uk "kubectl cp ~/yeet-data/assignment1-1.0-SNAPSHOT.jar cc-group2/bobuntu:/volume-mount-yeet; ~/spark-3.3.0-bin-hadoop3/bin/spark-submit \
+    --master k8s://https://128.232.80.18:6443 \
+    --deploy-mode cluster \
+    --name group2-wordcount \
+    --class com.cloudcomputing.group2.assignment1.App \
+    --conf spark.executor.instances=1 \
+    --conf spark.kubernetes.namespace=cc-group2 \
+    --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+    --conf spark.kubernetes.container.image=andylamp/spark:v3.3.0 \
+    --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
+    --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
+    --conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
+    --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
+    --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
+    --conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
+    local:///test-data/assignment1-1.0-SNAPSHOT.jar /test-data/data100MB.txt"
