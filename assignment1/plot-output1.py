4541d10d2c022ed8da851a6ff8eaf8bdca49a03d
nicole <nwyc2@cam.ac.uk>
2022-11-07 16:34:48 +0000

    graph stuff

---
+++ assignment1/plot-output1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sys
+import os
+
+def main():
+    argv = ["20221103T232402"]
+    # if len(argv) < 1:
+    #     print("ERROR: expecting timestamp")
+    #     sys.exit(1)
+
+    dirpath = "./experiments/"+argv[0]
+
+    dfmean = pd.DataFrame()
+    dfstd = pd.DataFrame()
+
+    # Number of Executors [2, timefor100, timefor200, timefor500]
+
+    for name in ['data-100MB', 'data-200MB', 'data-500MB']:
+        df1 = pd.read_csv(dirpath+"/measurements/"+name+".csv")
+        print(df1)
+        df1.set_index('Workers', inplace=True)
+        dfmean[name] = df1.mean(axis=1)
+        dfstd[name] = df1.std(axis=1)
+    
+    print(dfstd)
+
+    dfmean.plot(kind='bar', stacked=False, yerr = dfstd)
+    plt.ylabel("Time (s)")
+    plt.show()
+
+    
+
+if __name__ == "__main__":
+    main()
+    # main(sys.argv[1:])
