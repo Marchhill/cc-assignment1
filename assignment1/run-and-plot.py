10cf11cd91a7fc4f23110c4a3659e19c69ea4069
Jedidiah Koh <jedidiahkkh@gmail.com>
2022-11-08 21:04:36 +0000

    create experiments folder if missing

--- assignment1/run-and-plot.py
+++ assignment1/run-and-plot.py
@@ -9,6 +9,10 @@ today = datetime.now()
 # strip away separators and milliseconds
 iso = today.isoformat().replace("-", "").replace(":", "").replace(".", "")[:-6]
 
+# create the experiments folder if it doesn't exist
+if not os.path.isdir('./experiments'):
+    os.mkdir('./experiments')
+
 if os.path.isdir(f'./experiments/{iso}'):
     print('ERROR: folder with same timestamp already exists')
     sys.exit(1)
