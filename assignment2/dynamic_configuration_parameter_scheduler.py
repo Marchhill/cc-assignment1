770db1c8cc80249d1d1ad59eedda9f1e5251e7e4
Marc Harvey-Hill <Marchhill@users.noreply.github.com>
2022-11-22 14:53:20 +0000

    flexibly change which parameters are being tested

--- assignment2/dynamic_configuration_parameter_scheduler.py
+++ assignment2/dynamic_configuration_parameter_scheduler.py
@@ -4,94 +4,130 @@ import math
 
 # params: executorAllocationRation, batch.delay, initialExecutors
 
-RANGES = ((0.75, 1.0), (1., 7.0), (1.0, 11.0))
+RANGES = {
+	'executorAllocationRation': (0.75, 1.0),
+	'batch.delay': (500., 7000.0),
+	'initialExecutors': (1.0, 11.0)
+}
+
+ISFLOAT = {
+	'executorAllocationRation': True,
+	'batch.delay': False,
+	'initialExecutors': False
+}
+
 INITIAL_STEP_SIZE = 0.01
 LEARNING_RATE_CONSTANT = 0.02
 
 def randomRange(r):
 	return (random.random() * (r[1] - r[0])) + r[0]
 
-def makeStep(start, step, ranges):
-	res = [0.0, 0.0, 0.0]
+def makeStep(start, step, toTest, ranges):
+	res = {}
 
-	for i in range(0, 3):
-		res[i] = start[i] + step[i]
-		lo = ranges[i][0]
-		hi = ranges[i][1]
-		while res[i] < lo or res[i] > hi:
-			if res[i] < lo:
-				res[i] = lo + (lo - res[i])
-			elif res[i] > hi:
-				res[i] = hi - (res[i] - hi)
+	for param in toTest:
+		res[param] = start[param] + step[param]
+		lo = ranges[param][0]
+		hi = ranges[param][1]
+		while res[param] < lo or res[param] > hi:
+			if res[param] < lo:
+				res[param] = lo + (lo - res[param])
+			elif res[param] > hi:
+				res[param] = hi - (res[param] - hi)
 	
-	return tuple(res)
+	return res
 
-def randomVector(length, ranges):
-	vec = [random.gauss(0, 1) for i in range(3)]
-	mag = sum(x**2 for x in vec) ** 0.5
-	return tuple([vec[i] * length * (ranges[i][1] - ranges[i][0]) / mag for i in range(3)])
+def randomVector(length, toTest, ranges):
+	vec = {}
+	mag = 0
+
+	for param in toTest:
+		r = random.gauss(0, 1)
+		vec[param] = r
+		mag += r**2
+
+	mag = mag ** 0.5
+
+	for param in toTest:
+		vec[param] *= length * (ranges[param][1] - ranges[param][0]) / mag
+
+	return vec
 
 # use history to calculate the new params
-def updateParams(h, ranges, learningRate):
+def updateParams(h, toTest, toIgnore, ranges, learningRate):
 	if len(h) == 0:
 		# base case - generate random parameter
-		# todo: fix to be
-		return (randomRange(ranges[0]), randomRange(ranges[1]), randomRange(ranges[2]))
+		params = {}
+		for param in toTest:
+			params[param] = randomRange(ranges(param))
+		for param in toIgnore:
+			params[param] = ranges[param][0]
+		
+		return params
 	elif len(h) == 1:
 		# base case - take a random step
-		return makeStep(h[0][0], randomVector(INITIAL_STEP_SIZE, ranges), ranges)
+		return makeStep(h[0][0], toTest, randomVector(INITIAL_STEP_SIZE, toTest, ranges), ranges)
 	else:
 		# take step based on previous
 		deltaRes = h[-1][1] - h[-2][1]
-		step = [0.0, 0.0, 0.0]
+		step = {}
 
-		for i in range(0, 3):
-			delta = h[-1][0][i] - h[-2][0][i]
-			step[i] = -deltaRes * learningRate * (ranges[i][1] - ranges[i][0]) / delta
+		for param in toTest:
+			delta = h[-1][0][param] - h[-2][0][param]
+			step[param] = -deltaRes * learningRate * (ranges[param][1] - ranges[param][0]) / delta
 		
-		return makeStep(h[-1][0], tuple(step), ranges)
+		return makeStep(h[-1][0], step, ranges)
 
-params = updateParams([], RANGES, LEARNING_RATE_CONSTANT)
-history = []
+def getParam(params, name):
+	x = params[name]
+	x = x if ISFLOAT[name] else math.floor(x)
+	return str(x)
 
-for i in range(1, 21):
-	print("trying params: " + str(params))
-	'''
-	out = subprocess.run(('/usr/bin/time --format %e ./spark-3.3.0-bin-hadoop3/bin/spark-submit \
-				--master k8s://https://128.232.80.18:6443 \
-				--deploy-mode cluster \
-				--name group2-wordcount \
-				--class com.cloudcomputing.group2.assignment1.App \
-				--conf spark.kubernetes.namespace=cc-group2 \
-				--conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
-				--conf spark.kubernetes.container.image=andylamp/spark:v3.3.0 \
-				--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
-				--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
-				--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
-				--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
-				--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
-				--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
-				--conf spark.dynamicAllocation.enabled=true \
-				--conf spark.dynamicAllocation.shuffleTracking.enabled=true \
-				--conf spark.dynamicAllocation.executorAllocationRatio=' + str(params[0]) + ' \
-				--conf spark.kubernetes.allocation.batch.delay=' + str(math.floor(params[1])) + 's \
-				--conf spark.dynamicAllocation.initialExecutors=' + str(math.floor(params[2])) + ' \
-				local:///test-data/assignment1-1.0-SNAPSHOT.jar /test-data/data200MBtxt').split(),capture_output=True).stderr
-	
-	t = float(out.decode('utf-8').strip())
-	'''
-	t = params[0]**2 + params[1]**2 + params[2]**2
-	print("t: " + str(t))
+def chooseParams(options):
+	# toTest, toIgnore, history
+	return ([], [], [])
 
-	# create new timestamp folder
+def optimise(toTest, toIgnore, iter):
+	params = updateParams([], toTest, toIgnore, RANGES, LEARNING_RATE_CONSTANT)
+	history = []
 
-	#update history
-	history.append((params, t))
+	for i in range(1, iter + 1):
+		print("trying params: " + str(params))
+		out = subprocess.run(('/usr/bin/time --format %e ./spark-3.3.0-bin-hadoop3/bin/spark-submit \
+					--master k8s://https://128.232.80.18:6443 \
+					--deploy-mode cluster \
+					--name group2-wordcount \
+					--class com.cloudcomputing.group2.assignment1.App \
+					--conf spark.kubernetes.namespace=cc-group2 \
+					--conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+					--conf spark.kubernetes.container.image=andylamp/spark:v3.3.0 \
+					--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
+					--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
+					--conf spark.kubernetes.driver.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
+					--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.path=/test-data \
+					--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.mount.readOnly=false \
+					--conf spark.kubernetes.executor.volumes.persistentVolumeClaim.nfs-cc-group2.options.claimName=nfs-cc-group2 \
+					--conf spark.dynamicAllocation.enabled=true \
+					--conf spark.dynamicAllocation.shuffleTracking.enabled=true \
+					--conf spark.dynamicAllocation.executorAllocationRatio=' + getParam(params, 'executorAllocationRation') + ' \
+					--conf spark.kubernetes.allocation.batch.delay=' + getParam(params, 'batch.delay') + 'ms \
+					--conf spark.dynamicAllocation.initialExecutors=' + getParam(params, 'initialExecutors') + ' \
+					local:///test-data/assignment1-1.0-SNAPSHOT.jar /test-data/data-200MB.txt').split(),capture_output=True).stderr
+		
+		t = float(out.decode('utf-8').strip())
 
-	# update parameters
-	params = updateParams(history, RANGES, LEARNING_RATE_CONSTANT / i)
+		print("t: " + str(t))
 
-print("woo, finished!!")
+		# create new timestamp folder
+
+		#update history
+		history.append((params, t))
+
+		# update parameters
+		params = updateParams(history, toTest, toIgnore, RANGES, LEARNING_RATE_CONSTANT / i)
+
+toTest, toIgnore, history = chooseParams(['executorAllocationRation', 'batch.delay', 'initialExecutors'])
+history += optimise(toTest, toIgnore, 13)
 
 # write history to file
 for i in range(0, len(history)):
